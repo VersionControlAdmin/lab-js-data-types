5a52fdc4ef1016ceacbf517536896ff6a3b7a47f
Sebastian Luft <sebastian.luft.gm@gmail.com>
2024-09-18 14:56:02 +0200

    Done

--- index.js
+++ index.js
@@ -9,10 +9,20 @@ const s5 = "and";
 
 // Concatenate the string variables into one new string
 
+function tongueTwisterGenerator (wordArray) {
+    let tongueTwister ="";
+    for (i = 0; i < wordArray.length;i++) {
+        tongueTwister +=wordArray[i]+" ";
+    }
+    return tongueTwister;
+}
+
+let wordArrayInitialised = [s1,s2,s3,s4,s5];
+finalTongueTwister =tongueTwisterGenerator(wordArrayInitialised);
 
 // Print out the concatenated string
 
-
+console.log(finalTongueTwister);
 
 
 /*******************************************
@@ -23,10 +33,11 @@ const part2 = "script";
 
 // Convert the last letter of part1 and part2 to uppercase and concatenate the strings
 
+UppercaseConcattedString = part1.slice(0,3)+part1[3].toUpperCase()+part2.slice(0,5)+part2[5].toUpperCase();
 
 // Print the cameLtaiL-formatted string
 
-
+console.log(UppercaseConcattedString);
 
 
 /*******************************************
@@ -35,10 +46,10 @@ const part2 = "script";
 const billTotal = 84;
 
 // Calculate the tip (15% of the bill total)
-
+tipAmount=0.15*billTotal;
 
 // Print out the tipAmount
-
+console.log(tipAmount)
 
 
 
@@ -48,10 +59,11 @@ const billTotal = 84;
 
 // Generate a random integer between 1 and 10 (inclusive)
 
+randomNumber = Math.floor(Math.random()*10)+1;
 
 // Print the generated random number
 
-
+console.log(randomNumber);
 
 /*******************************************
     Iteration 3.1 | Booleans
@@ -61,16 +73,22 @@ const a = true;
 const b = false;
 
 // Try and guess the output of the below expressions first and write your answers down:
-const expression1 = a && b;
+const expression1 = a && b; //false
 
-const expression2 = a || b;
+const expression2 = a || b; //true
 
-const expression3 = !a && b;
+const expression3 = !a && b; //false
 
-const expression4 = !(a && b);
+const expression4 = !(a && b); //true
 
-const expression5 = !a || !b;
+const expression5 = !a || !b; //true
 
-const expression6 = !(a || b);
+const expression6 = !(a || b); //false
 
-const expression7 = a && a;
+const expression7 = a && a; // true
+
+expressionArray = [expression1,expression2,expression3,expression4,expression5,expression6,expression7]
+
+for(i=0;i<expressionArray.length;i++) {
+    console.log(expressionArray[i]);
+}
